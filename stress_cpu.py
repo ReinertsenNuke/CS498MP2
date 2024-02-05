d5b9fc9408e3b6f560c2272310931c4a11b8d8e6
ReinertsenNuke <107160502+ReinertsenNuke@users.noreply.github.com>
2024-02-04 23:54:24 -0500

    Add files via upload

--- stress_cpu.py
+++ stress_cpu.py
@@ -1,30 +1,17 @@
-from flask import Flask, request
 from multiprocessing import Pool
 from multiprocessing import cpu_count
-import socket
+import time
 
-app = Flask(__name__)
-
+# Do intensive computation to stress the CPU
 def stress_cpu(n):
-   total = 0
-   for i in range(n):
-      total += i**2
-   return total
+    total = 0
+    for i in range(n):
+        total += i**2
+    return total
 
 start_time = time.time()
-
+# Create as many as processes as there are CPU cores
 processes = cpu_count()
 pool = Pool(processes)
-print(pool.map(stress_cpu,[110000000,110000000]))
-print("time_cost: ", time.time() - start_time)
-
-@app.route('/',methods=['POST'])
-def do_post():
-   subprocess.Popen("stress_cpu.py")
-
-@app.route('/',methods=['GET'])
-   return socket.gethostname()
-
-if __name__ == '__main__'
-   app.run(host="0.0.0.0",port=5000)
-
+print(pool.map(stress_cpu, [110000000, 110000000]))
+print("time cost: ", time.time() - start_time)
